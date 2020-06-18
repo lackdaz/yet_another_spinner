1008e6880d464969b41e47152ce50d3b9e93635f
Seth Loh <lackdaz@gmail.com>
2020-06-19 00:08:50 +0800

    added some space between the eggs

--- examples/egg_hatching.sh
+++ examples/egg_hatching.sh
@@ -15,29 +15,29 @@ seq 0 1 $SET_TIMER | while read i; do
     printf '\r%2s' ${i}
     echo -n 's '
     if [ $((i % 12)) -eq 0 ]; then
-        echo -en "🥚  "
+        echo -en "🥚    "
     elif [ $((i % 12)) -eq 1 ]; then
-        echo -en "🥚🥚 "
+        echo -en "🥚 🥚  "
     elif [ $((i % 12)) -eq 2 ]; then
-        echo -en "🥚🥚🥚"
+        echo -en "🥚 🥚 🥚"
     elif [ $((i % 12)) -eq 3 ]; then
-        echo -en "🐣🥚🥚"
+        echo -en "🐣 🥚 🥚"
     elif [ $((i % 12)) -eq 4 ]; then
-        echo -en "🐣🐣🥚"
+        echo -en "🐣 🐣 🥚"
     elif [ $((i % 12)) -eq 5 ]; then
-        echo -en "🐣🐣🐣"
+        echo -en "🐣 🐣 🐣"
     elif [ $((i % 12)) -eq 6 ]; then
-        echo -en "🐥🐣🐣"
+        echo -en "🐥 🐣 🐣"
     elif [ $((i % 12)) -eq 7 ]; then
-        echo -en "🐥🐥🐣"
+        echo -en "🐥 🐥 🐣"
     elif [ $((i % 12)) -eq 8 ]; then
-        echo -en "🐥🐥🐥"
+        echo -en "🐥 🐥 🐥"
     elif [ $((i % 12)) -eq 9 ]; then
-        echo -en " 🐥🐥"
+        echo -en "  🐥 🐥"
     elif [ $((i % 12)) -eq 10 ]; then
-        echo -en "  🐥"
+        echo -en "    🐥"
     else
-        echo -en "   "
+        echo -en "     "
     fi
     echo -n '  '
     sleep 0.25
